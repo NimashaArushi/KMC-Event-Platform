ad0bf038e09e02acd60200e036770ccb832578bb
arushi <arushinimasha@gmail.com>
2026-07-31 18:19:29 +0530

    Added UserRepository, AuthService, and updated Web.config connection string

---
+++ Data/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using Kmc_Login.Models;
+
+namespace Kmc_Login.Data
+{
+    public class UserRepository
+    {
+        private string connStr = ConfigurationManager.ConnectionStrings["KMCConnString"].ConnectionString;
+
+        public bool RegisterUser(User user)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr)) {
+                string query = "INSERT INTO Users(Email,Password) VALUES (@Email,@Password)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+
+
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+
+        public bool IsEmailExists(string email) {
+            using (SqlConnection conn = new SqlConnection(connStr)) {
+                string query = "SELECT COUNT(1) FROM Users WHERE Email = @Email";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                return count > 0;
+            
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using Kmc_Login.Data;
+using Kmc_Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kmc_Login.Services
+{
+    public class AuthService
+    {
+        private UserRepository _userRepo = new UserRepository();
+
+        public string Register(string email, string password, string confirmPassword) {
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password)) {
+                return "Plz fill in all fields ❌";
+            }
+            if (password != confirmPassword) {
+                return "passwords do not match ‼️";
+            }
+
+            if (_userRepo.IsEmailExists(email)) {
+                return "Email is already registered!";
+            }
+
+            User newUser = new User
+            {
+                Email = email,
+                Password = password
+            }; bool isSuccess = _userRepo.RegisterUser(newUser);
+
+            if (isSuccess)
+            {
+                return "SUCCESS";
+            }
+            else
+            {
+                return "Failed to register. Please try again.";
+            }
+        }
+    }
+}
